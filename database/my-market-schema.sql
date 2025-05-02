5e2b00d6bcd87fe3fc1df3e934a44f7dcc81e550
Daniel Augusto <daniel.aug.souza@gmail.com>
2025-05-02 01:16:06 -0300

    Upd docker-compose for mysql | Upd db schema

--- database/my-market-schema.sql
+++ database/my-market-schema.sql
@@ -1,9 +1,11 @@
-create database if not exists my-market;
+create database if not exists my_market;
+
+use my_market;
 
 create table if not exists product (
   id int auto_increment,
   name varchar(255),
   description varchar(255),
   price float(9,2),
-  PRIMARY KEY id
+  PRIMARY KEY (id)
 );
