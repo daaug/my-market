d6927d0353aa7a1c4dc4c10474b6c803f2eb949a
Daniel Augusto <daniel.aug.souza@gmail.com>
2025-05-01 23:58:28 -0300

    Create database schema

---
+++ database/my-market-schema.sql
@@ -0,0 +1,9 @@
+create database if not exists my-market;
+
+create table if not exists product (
+  id int auto_increment,
+  name varchar(255),
+  description varchar(255),
+  price float(9,2),
+  PRIMARY KEY id
+);
